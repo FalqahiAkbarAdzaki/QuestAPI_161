904c230bd4f1b93b888fabea1490bb76fe9fa904
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-12-19 07:42:34 +0700

    Add library & dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
 
     implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
